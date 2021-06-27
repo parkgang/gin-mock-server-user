3c70f1edf0ad2d50eb1d63f9f833ba24839ef993
박경은 <ruddms936@naver.com>
2021-06-27 15:44:40 +0900

    use: react-router-dom

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,7 +1,39 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+
 export default function App() {
   return (
-    <div>
-      <span>Hello, world</span>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
