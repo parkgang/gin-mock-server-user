e60b68b16f3ee5f115d8db2f340a076b265c0e18
박경은 <ruddms936@naver.com>
2021-06-27 19:03:37 +0900

    use: 필수 라이브러리

--- webapp/README.md
+++ webapp/README.md
@@ -4,8 +4,10 @@
 1. create-react-app
 1. react-router-dom
 1. react-query
+1. react-error-boundary
 1. recoil
 1. @fluentui/react-northstar
+1. axios
 
 # cra
 
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
 
 import reportWebVitals from "./reportWebVitals";
 
