c65b79b6d10e67b5b60716ca221e039ee048b1df
박경은 <ruddms936@naver.com>
2021-06-27 21:31:20 +0900

    fix: CSR Router 가능 하도록 수정

--- server/main.go
+++ server/main.go
@@ -14,8 +14,18 @@ func main() {
 
 	router := gin.Default()
 
+	// SPA
+	{
+		// 정적파일 서빙
+		router.Use(static.Serve("/", static.LocalFile("../webapp/build", true)))
+		// [CSR Router를 위함](https://github.com/gin-gonic/contrib/issues/90#issuecomment-286924994)
+		router.NoRoute(func(c *gin.Context) {
+			// 반드시 index.html 파일 경로로 지정해야합니다.
+			c.File("../webapp/build/index.html")
+		})
+	}
+
 	router.Use(cors.Default())
-	router.Use(static.Serve("/", static.LocalFile("../webapp/build", true)))
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
