00e9a1a813e567270cb792e19b185bc984b2bb2e
박경은 <ruddms936@naver.com>
2021-07-02 22:49:14 +0900

    up: 미완성된 코드 삭제

--- webapp/src/components/ErrorFallback/ErrorFallback.tsx
+++ webapp/src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,4 +1,3 @@
-import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { Flex, Text, Segment, ErrorIcon } from "@fluentui/react-northstar";
 
@@ -12,8 +11,6 @@ type Props = {
  * 각 에러 instance에 맞는 적절한 컴포넌트를 바인딩 해줍니다.
  */
 function ErrorFallback({ title = "페이지를 표시하는 도중 문제가 발생했습니다.", error }: Props) {
-  const history = useHistory();
-
   // TODO: error instance에 맞게 컴포넌트 바인딩
   // handleError(); 등 을 통해 error를 any 타입으로 던지는 경우 Error interface 를 충족하지 못하여 문제가 발생합니다.
   // 모든 에러를 처리하는 Component으로 디자인 되었으므로 방어적으로 처리합니다.
@@ -22,9 +19,7 @@ function ErrorFallback({ title = "페이지를 표시하는 도중 문제가 발
     error = new Error(error);
   }
   if (axios.isAxiosError(error)) {
-    if (error.response?.data.match(/\binteraction required\b/) !== null) {
-      history.push(`/login`);
-    }
+    // TODO: 서버 에러의 http statue에 맞게 핸들링 하도록 추가하기
   }
 
   return (
