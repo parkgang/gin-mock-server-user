4a12e8454ea021e1b1234342b680d79b1a621882
박경은 <ruddms936@naver.com>
2021-07-02 20:16:26 +0900

    fix: 브라우저 요청은 엄격하게 검사하므로 / 모두 삭제

--- server/router/router.go
+++ server/router/router.go
@@ -9,11 +9,11 @@ func Use(api *gin.RouterGroup) {
 	api.GET("/ping", controller.Ping)
 	users := api.Group("/users")
 	{
-		users.POST("/", controller.PostUser)
-		users.GET("/", controller.GetAllUser)
+		users.POST("", controller.PostUser)
+		users.GET("", controller.GetAllUser)
 		users.GET("/:id", controller.GetUser)
 		users.PUT("/:id", controller.PutUser)
-		users.DELETE("/", controller.DeleteAllUser)
+		users.DELETE("", controller.DeleteAllUser)
 		users.DELETE("/:id", controller.DeleteUser)
 	}
 }
