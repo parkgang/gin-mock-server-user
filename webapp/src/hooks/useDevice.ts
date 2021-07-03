f9c41e69b2dd1477ffce56173cc2fd20879ca184
박경은 <ruddms936@naver.com>
2021-07-03 15:21:48 +0900

    edit: 간단한 미디어 쿼리만 셋팅

--- webapp/src/hooks/useDevice.ts
+++ webapp/src/hooks/useDevice.ts
@@ -1,11 +1,15 @@
 import { useMediaQuery } from "react-responsive";
 
 export default function useDevice() {
-  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-  const isTablet = useMediaQuery({ query: "(min-width: 768px) and (max-width: 1023px)" });
-  const isMobileOrTablet = useMediaQuery({ query: "(max-width: 1023px)" });
-  const isPC = useMediaQuery({ query: "(min-width: 1024px) and (max-width: 1279px)" });
-  const isPCWide = useMediaQuery({ query: "(min-width: 1280px)" });
-  const isPCOrPCWide = useMediaQuery({ query: "(min-width: 1024px)" });
-  return { isMobile, isTablet, isMobileOrTablet, isPC, isPCWide, isPCOrPCWide };
+  const isDesktop = useMediaQuery({ minWidth: 992 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isNotMobile = useMediaQuery({ minWidth: 768 });
+
+  return {
+    isDesktop,
+    isTablet,
+    isMobile,
+    isNotMobile,
+  };
 }
