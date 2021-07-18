3674bafbd6192a64596b05211d653fb60c57193a
박경은 <ruddms936@naver.com>
2021-07-19 01:26:28 +0900

    up: 컴포넌트 종속성 분리

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,14 +1,29 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Flex } from "@fluentui/react-northstar";
 
-import UserList, { Header } from "components/UserList";
+import { LoadingCard } from "components/Loading";
+import ErrorFallback from "components/ErrorFallback";
+import { UserCard, Header } from "components/UserList";
+
+import { handlerOnError } from "libs/error";
 
 export default function Home() {
   return (
     <>
-      <Flex column gap="gap.small">
-        <Header />
-        <UserList />
-      </Flex>
+      <ErrorBoundary
+        fallbackRender={({ error }) => (
+          <ErrorFallback title="홈 페이지에서 문제가 발생했어요!" error={error} />
+        )}
+        onError={handlerOnError}
+      >
+        <Flex column gap="gap.small">
+          <Header />
+          <Suspense fallback={<LoadingCard />}>
+            <UserCard />
+          </Suspense>
+        </Flex>
+      </ErrorBoundary>
     </>
   );
 }
