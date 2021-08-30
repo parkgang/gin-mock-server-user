82a86992a3b9f71a268346fea00c4b0968022c46
박경은 <ruddms936@naver.com>
2021-08-31 07:30:47 +0900

    refactor: 복수 디렉터리에 s 추가

--- server/router/router.go
+++ server/router/router.go
@@ -2,18 +2,18 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/parkgang/modern-board/controller"
+	"github.com/parkgang/modern-board/controllers"
 )
 
 func Use(api *gin.RouterGroup) {
-	api.GET("/ping", controller.Ping)
+	api.GET("/ping", controllers.Ping)
 	users := api.Group("/users")
 	{
-		users.POST("", controller.PostUser)
-		users.GET("", controller.GetAllUser)
-		users.GET("/:id", controller.GetUser)
-		users.PUT("/:id", controller.PutUser)
-		users.DELETE("", controller.DeleteAllUser)
-		users.DELETE("/:id", controller.DeleteUser)
+		users.POST("", controllers.PostUser)
+		users.GET("", controllers.GetAllUser)
+		users.GET("/:id", controllers.GetUser)
+		users.PUT("/:id", controllers.PutUser)
+		users.DELETE("", controllers.DeleteAllUser)
+		users.DELETE("/:id", controllers.DeleteUser)
 	}
 }
