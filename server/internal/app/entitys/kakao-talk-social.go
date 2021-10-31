cb8c7d698c4b380bc193ed6931db2809979ea9c6
박경은 <ruddms936@naver.com>
2021-11-01 01:50:29 +0900

    up: 주석 업데이트

--- server/internal/app/entitys/kakao-talk-social.go
+++ server/internal/app/entitys/kakao-talk-social.go
@@ -4,5 +4,5 @@ type KakaoTalkSocial struct {
 	Id             uint   `gorm:"primary_key;"`
 	Email          string `gorm:"unique;not null;size:50;"`
 	NickName       string `gorm:"not null;size:20;"`
-	ThumbnailImage string `gorm:"size:2083;"`
+	ThumbnailImage string `gorm:"size:2083;"` // 이미지의 url을 저장하기 위한 최대 크기로 잡아놓았습니다.
 }
