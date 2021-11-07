23562f4930753b22b47c512f05aaf7759e1c5466
박경은 <ruddms936@naver.com>
2021-11-07 19:23:34 +0900

    add: localstorage 삭제 추가

--- webapp/src/libs/local-storage.ts
+++ webapp/src/libs/local-storage.ts
@@ -13,7 +13,6 @@ const refreshTokenKey = `${name}-refreshToken`;
 export function getLocalStorageTheme() {
   return getStringToFluentuiNorthstarTheme(localStorage.getItem(themeKey));
 }
-
 export function setLocalStorageTheme(theme: ThemePrepared) {
   localStorage.setItem(themeKey, getFluentuiNorthstarThemeToString(theme));
 }
@@ -21,15 +20,19 @@ export function setLocalStorageTheme(theme: ThemePrepared) {
 export function getLocalStorageAccessToken() {
   return localStorage.getItem(accessTokenKey);
 }
-
 export function setLocalStorageAccessToken(token: string) {
   localStorage.setItem(accessTokenKey, token);
+}
+export function delLocalStorageAccessToken(token: string) {
+  localStorage.removeItem(accessTokenKey);
 }
 
 export function getLocalStorageRefreshToken() {
   return localStorage.getItem(refreshTokenKey);
 }
-
 export function setLocalStorageRefreshToken(token: string) {
   localStorage.setItem(refreshTokenKey, token);
+}
+export function delLocalStorageRefreshToken(token: string) {
+  localStorage.removeItem(refreshTokenKey);
 }
